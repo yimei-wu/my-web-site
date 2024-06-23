d8ec6511620e872c45ec56834d29ae9b72a760ac
yimei-wu <wuyimei60@gmail.com>
2024-06-23 19:02:06 +0200

    add button next as a boolean

--- single.php
+++ single.php
@@ -1,35 +1,15 @@
 <?php
-/**
- * single.php si occupa di visualizzare un singolo post
- * o custom post type.
- *
- * Se il post è protetto da password, viene renderizzato
- * single-password.twig.
- *
- * Altrimenti viene renderizzato il primo file `.twig`
- * trovato in ordine di priorità:
- * - `single-{id}.twig`
- * - `single-{slug}.twig`
- * - `single-{post_type}.twig`
- * - `single.twig`
- */
 
 use Timber\Timber;
 
 $context = Timber::context();
-// $context["post"] = $timber_post;
 
 $context["post"] = Timber::get_post();
 
-// Timber::render(
-//     [
-//         "single-" . $timber_post->ID . ".twig",
-//         "single-" . $timber_post->slug . ".twig",
-//         "single-" . $timber_post->post_type . ".twig",
-//         "single.twig",
-//     ],
-//     $context
-// );
+$next_post = get_next_post();
+if ($next_post) {
+    $context["next_post"] = Timber::get_post($next_post->ID);
+}
 
 Timber::render(
     ["single-" . $context["post"]->post_type . ".twig", "single.twig"],
